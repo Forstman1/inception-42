dcb5f552c9666f84a8db3a8d3a8d25126e27074e
Sami Hafid <sahafid@e2r10p14.1337.ma>
2022-12-23 15:05:24 +0100

    fixing configuration process for wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -32,40 +32,54 @@ cd /var/www/html
 # cd /
 
 
-export WP_DATABASE_NAME=wordpress
-export WP_DATABASE_USR=sami
-export WP_DATABASE_PWD=nice
-export WP_DATABASE_HOST=mariadb
+# export WP_DATABASE_NAME=wordpress
+# export WP_DATABASE_USR=sami
+# export WP_DATABASE_PWD=nice
+# export WP_DATABASE_HOST=mariadb
 
-export DOMAIN_NAME=sahafid.1337.ma
-export WP_TITLE=RiadElYacoute
-export WP_ADMIN_USR=forstman
-export WP_ADMIN_PWD=123
-export WP_ADMIN_EMAIL=sami.hafid.hs@gmail.com
-export WP_USR=sami
-export WP_EMAIL=sahafid@1337.com
-export WP_PWD=123
+# export DOMAIN_NAME=sahafid.1337.ma
+# export WP_TITLE=RiadElYacoute
+# export WP_ADMIN_USR=forstman
+# export WP_ADMIN_PWD=123
+# export WP_ADMIN_EMAIL=sami.hafid.hs@gmail.com
+# export WP_USR=sami
+# export WP_EMAIL=sahafid@1337.com
+# export WP_PWD=123
 
 # source of info https://developer.wordpress.org/cli/commands/core/
+rm -rf *
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+
 chmod +x wp-cli.phar 
+
 mv wp-cli.phar /usr/local/bin/wp
+
 
 wp core download --allow-root
 
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php 
 
-
-wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 mv /wp-config.php /var/www/html/wp-config.php
-# wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+
+
+sed -i -r "s/db1/$db_name/1"      wp-config.php
+sed -i -r "s/user/$db_user/1"      wp-config.php
+sed -i -r "s/pwd/$db_pwd/1"      wp-config.php
+
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+
+
+
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
 
 wp theme install astra --activate --allow-root
 
 
 wp plugin install redis-cache --activate --allow-root
+
 wp plugin update --all --allow-root
 
 
@@ -80,9 +94,6 @@ cd /
 
 
 
-
-wp plugin install redis-cache --activate --allow-root
-wp plugin update --all --allow-root
 
 wp redis enable --allow-root
 
