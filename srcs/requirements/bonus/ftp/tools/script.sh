2db79640375704fd3cd783f87c4b2471ee35c0a0
Sami Hafid <sahafid@e2r10p13.1337.ma>
2022-12-25 21:22:49 +0100

    working on ftp issues

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -3,6 +3,7 @@
 
 mkdir -p /var/www/html
 
+service vsftpd start
 # cp /etc/vsftpd/vsftpd.conf /etc/vsftpd/vsftpd.conf.bak
 # mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
 
@@ -14,8 +15,26 @@ echo "sami:nice" | /usr/sbin/chpasswd &> /dev/null
 
 chown -R sami:sami /var/www/html
 
-echo "sami" | tee -a /etc/vsftpd/vsftpd.userlist &> /dev/null
+echo "sami" | tee -a /etc/vsftpd.userlist &> /dev/null
 
-/usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+# /usr/sbin/vsftpd /etc/vsftpd.conf
 
+mkdir /home/sami/ftp
+
+mkdir /home/sami/ftp/files
+chown sami:sami /home/sami/ftp/files
+
+sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
+sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
+
+echo "user_sub_token=sami
+local_root=/home/sami/ftp
+pasv_min_port=40000
+pasv_max_port=50000
+userlist_enable=YES
+userlist_file=/etc/vsftpd.userlist
+userlist_deny=NO" >> /etc/vsftpd.conf
+
+service vsftpd stop
+/usr/sbin/vsftpd
 sleep 10000000
