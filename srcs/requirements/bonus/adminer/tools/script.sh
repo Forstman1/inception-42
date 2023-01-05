af94f6d56b339a0086d2662360304f2ea340cf65
Sami Hafid <sahafid@e2r10p13.1337.ma>
2023-01-05 12:20:34 +0100

    adding some necessarily stuff and updating readme

--- srcs/requirements/bonus/adminer/tools/script.sh
+++ srcs/requirements/bonus/adminer/tools/script.sh
@@ -5,8 +5,6 @@ wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
 chown -R www-data:www-data /var/www/html/adminer.php 
 chmod 755 /var/www/html/adminer.php
 
-rm /var/www/html/index.html
 
-service apache2 start
-
-sleep 1000000
+cd /var/www/html
+php -S 0.0.0.0:80
--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -6,15 +6,17 @@ service vsftpd start
 # Add the USER, change his password and declare him as the owner of wordpress folder and all subfolders
 
 adduser $ftp_user --disabled-password
+# adduser sami --disabled-password
 
-echo "$ftp_user:$ftp_pwd" | /usr/sbin/chpasswd &> /dev/null
+echo "$ftp_user:$ftp_pwd" | /usr/sbin/chpasswd
 
-# chown -R $ftp_user:$ftp_user /var/www/html
 
-echo "$ftp_user" | tee -a /etc/vsftpd.userlist &> /dev/null
+echo "$ftp_user" | tee -a /etc/vsftpd.userlist 
 
 
 mkdir /home/$ftp_user/ftp
+
+# mkdir /home/sami/ftp
 
 chown nobody:nogroup /home/$ftp_user/ftp
 chmod a-w /home/$ftp_user/ftp
